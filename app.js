177d4813fdd653ebf6352b71c925a556bebc4add
marjo1992 <verelmarjolaine@hotmail.fr>
2023-07-10 16:48:09 +0200

    added sections

--- app.js
+++ app.js
@@ -1,7 +1,91 @@
 const AXIOS = axios.create({baseURL: 'http://pinkcactus.freeboxos.fr/pmback'})
 
-new Vue({
-	el: '#app',
+Vue.component('home', {
+	template: `<div>home</div>`
+})
+
+Vue.component('places', {
+	template: `<div id="places">
+		<h1 class="info-title">Lieux</h1>
+		<div class="info-content">
+			<ul>
+				<li style="font-size: 1.3rem"><span style="font-weight: bold">Chez nous</span><span class="tab">12 rue de la tour, 31380 Gragnague</span></li>
+				<li style="font-size: 1.3rem"><span style="font-weight: bold">Mairie de Gragnague</span><span class="tab">15 Pl. Bellegarde, 31380 Gragnague</span></li>
+				<li style="font-size: 1.3rem"><span style="font-weight: bold">Église de Gragnague</span><span class="tab">19 Pl. Bellegarde, 31380 Gragnague</span></li>
+				<li style="font-size: 1.3rem"><span style="font-weight: bold">Restaurant "le petit café"</span><span class="tab">14 Av. du champ de Foire, 31380 Gragnague</span></li>
+			</ul>
+			<img src="assets/plan gragnague.png" width="100%">
+		</div>
+	</div>`
+})
+
+Vue.component('program', {
+	template: `<div id="program">
+		<h1 class="info-title">Programme</h1>
+		<ul class="info-content">
+			<li>
+				<img src="assets/mairie.png" height="100px">
+				<div>
+					<h2>Cérémonie civile</h2>
+					<div>15h30</div>
+					<div>Mairie de Gragnague</div>
+				</div>
+			</li>
+			<li>
+				<img src="assets/photos.png" height="100px">
+				<div>
+					<h2>Photos</h2>
+				</div>
+			</li>
+			<li>
+				<img src="assets/église.png" height="100px">
+				<div>
+					<h2>Cérémonie religieuse</h2>
+					<div>16h30</div>
+					<div>Église de Gragnague</div>
+				</div>
+			</li>
+			<li>
+				<img src="assets/apéro.png" height="100px">
+				<div>
+					<h2>Vin d'honneur</h2>
+					<div>Chez nous</div>
+				</div>
+			</li>
+			<li>
+				<img src="assets/couverts.png" height="100px">
+				<div>
+					<h2>Dinner</h2>
+					<div>20h30</div>
+					<div>Restaurant "le petit café"</div>
+				</div>
+			</li>
+			<li>
+				<img src="assets/musique.png" height="100px">
+				<div>
+					<h2>Soirée</h2>
+					<div>Chez nous</div>
+				</div>
+			</li>
+		</ul>
+	</div>`
+})
+
+Vue.component('photos', {
+	template: `<div class="info">
+		<div @click="openShare" class="share">
+			Vous pouvez partager vos photos du mariage en cliquant sur le bouton partager:
+			<div class="share-button">Partager</div>
+		</div>
+		<div v-if="files.length && files.some(f => !f.status)">Veuillez ne pas quitter la page tant que les fichiers ne sont pas tous uploadés ({{uploads}} / {{files.length}})</div>
+		<div v-else-if="files.length">Téléchargement terminé, merci <span class="material-symbols-outlined" style="color: red">favorite</span></div>
+		<div class="file-list">
+			<div v-for="file in files" class="file">
+				<span>{{file.name}}</span>
+				<span class="material-symbols-outlined" :style="{color: color(file.status)}">{{icon(file.status)}}</span>
+			</div>
+		</div>
+	</div>`,
 	data() {
 		return {
 			files: [],
@@ -11,9 +95,9 @@ new Vue({
 	methods: {
 		openShare() {
 			let input = document.createElement('input')
-		    	input.type = "file"
+			input.type = "file"
 	  		input.setAttribute("multiple","")
-		    	input.onchange = async () => {
+			input.onchange = async () => {
 				this.uploads = 0
 				this.files = [...input.files].map(f => ({name: f.name, status: 0}))
 				for (let i=0; i<input.files.length; ++i) {
@@ -27,11 +111,8 @@ new Vue({
 					} finally {
 						this.uploads++
 					}
-				}				
-
-				// [...input.files].forEach(file => fd.append(`files`, file))
-				// await AXIOS.post('/new-photo', fd)
-		    	}
+				}
+			}
 		  	input.click()
 		},
 		icon(val) {
@@ -48,5 +129,17 @@ new Vue({
 				case 2: return 'red'
 			}
 		}
+	}
+
+})
+
+new Vue({
+	el: '#app',
+	data() {
+		return {
+			route: 'home'
+		}
+	},
+	methods: {
 	}
 })
